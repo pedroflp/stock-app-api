e48a081b61304317b412d7b8a2d3ae568851716d
Pedroflp <pedrofelipe0940@gmail.com>
2021-03-08 18:21:58 -0300

    Fixing package.json, routes, procfile

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ import AuthController from './controllers/AuthController';
 const routes = Router()
 const upload = multer();
 
+routes.get('/', upload.none(), AuthController.authenticate);
 
 routes.post('/login', upload.none(), AuthController.authenticate)
 routes.post('/registrar', upload.none(), UsersController.store)
