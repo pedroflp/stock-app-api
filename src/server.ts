a105bd2290165428b2932cddbf3cf0ac2c6d5bff
Pedroflp <pedrofelipe0940@gmail.com>
2021-03-08 03:32:11 -0300

    Adding Procfile

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import routes from './routes';
 import errorHandler from './errors/handler';
 
 import './database/connection';
-import authMiddleware from './middlewares/authMiddleware';
 
 const app = express();
 
